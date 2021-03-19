b82f95d09f04f9f712e5a2c748065bfadaa7737c
Sandeep-Charan <sandeepcharank@gmail.com>
2021-03-19 19:31:42 +0530

    Updates

---
+++ precache-manifest.e9a343733c0d4e25b124a79ef669aea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b15c033fef3d315fbb185566a8d9d5",
+    "url": "/Razorpay-Integration/index.html"
+  },
+  {
+    "revision": "4c4a4ba5138ade7f43e7",
+    "url": "/Razorpay-Integration/static/css/main.b162d269.chunk.css"
+  },
+  {
+    "revision": "6122f5393b35aa0c59b9",
+    "url": "/Razorpay-Integration/static/js/2.1b395ea8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Razorpay-Integration/static/js/2.1b395ea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c4a4ba5138ade7f43e7",
+    "url": "/Razorpay-Integration/static/js/main.803b1bc4.chunk.js"
+  },
+  {
+    "revision": "d4ec204b407993e023d4",
+    "url": "/Razorpay-Integration/static/js/runtime-main.4390384c.js"
+  }
+]);
